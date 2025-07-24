851afe4e73dfd46d530100517894347b6e84761a
Mushieh Edison <mushiehedison66@gmail.com>
2025-07-24 16:38:56 +0100

    new commit

--- track2/HealthApp/__init__.py
+++ track2/HealthApp/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     # Use absolute path for site.db
     db_path = os.path.join(os.path.dirname(__file__), 'site.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     logging.debug(f"Database path: {db_path}")
     db.init_app(app)
